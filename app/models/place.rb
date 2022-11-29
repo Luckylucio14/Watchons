5d01ad6bc6e6eb302b918a79786393f1b20e8f97
Ruben HERNANDEZ DOMINGUEZ <113209489+rlucky87@users.noreply.github.com>
2022-11-29 15:47:02 +0100

    seedfinish

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,3 +1,4 @@
 class Place < ApplicationRecord
+  has_one_attached :photo
   has_many :events, through: :event_places
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,5 @@
 class Team < ApplicationRecord
-  belongs_to :events, through: :event_teams
+  has_one_attached :photo
+  has_many :event_teams
+  has_many :events, through: :event_teams
 end
