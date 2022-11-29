ff4fbdd5531136a3c77613e5309027283fb20796
Luc Randriamanantseheno <luckylucio14@gmail.com>
2022-11-29 17:20:18 +0100

    fix seeds

--- db/migrate/20221129145207_addname_to_users.rb
+++ db/migrate/20221129145207_addname_to_users.rb
@@ -1,7 +1,7 @@
 class AddnameToUsers < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, first_name, :string
-    add_column :users, last_name, :string
-    rename_column :users, caracteristic, characteristic 
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    rename_column :places, :caracteristic, :characteristic 
   end
 end
