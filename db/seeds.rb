9a4ea19d3ed9f559133903e86808a5621f8e9995
Luc Randriamanantseheno <luckylucio14@gmail.com>
2022-11-29 11:01:51 +0100

    structure seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "open-uri"
+
+puts 'Cleaning database...'
+Place.destroy_all
+Event.destroy_all
+Match.destroy_all
+
+puts 'Creating Users...'
+# USERS
+
+Anakin = User.create!(competition:"", localisation:"", heure:"", date:"", channel:"")
+Anakin.photo.attach(io: )
+
+
+puts 'Creating Events...'
+# EVENTS
+match1 = Match.create!(competition:"", localisation:"", heure:"", date:"", channel:"")
+match1 = Match.create!(competition:"", localisation:"", heure:"", date:"", channel:"")
+match1 = Match.create!(competition:"", localisation:"", heure:"", date:"", channel:"")
+match1 = Match.create!(competition:"", localisation:"", heure:"", date:"", channel:"")
+match1 = Match.create!(competition:"", localisation:"", heure:"", date:"", channel:"")
+match1 = Match.create!(competition:"", localisation:"", heure:"", date:"", channel:"")
+match1 = Match.create!(competition:"", localisation:"", heure:"", date:"", channel:"")
+match1 = Match.create!(competition:"", localisation:"", heure:"", date:"", channel:"")
+match1 = Match.create!(competition:"", localisation:"", heure:"", date:"", channel:"")
+match1 = Match.create!(competition:"", localisation:"", heure:"", date:"", channel:"")
+
+
+puts 'Creating Places...'
+# PLACES
