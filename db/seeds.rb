053f9da136b6e6603c87d8e31e56d8d0d9b0a20d
Luc Randriamanantseheno <luckylucio14@gmail.com>
2022-11-29 14:51:29 +0100

    create seeds places

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,23 +17,23 @@ puts 'Creating Users...'
 
 william = User.create!(first_name:"William", last_name:"Wallace", email:"william.wallace@gmail.com", password:"123456")
 file = URI.open(http://idata.over-blog.com/0/47/62/15/articles/braveheart02.jpg)
-william.photo.attach(io: file filename:"william.png" content_type:"image/png")
+william.photo.attach(io: file, filename:"william.png", content_type:"image/png")
 
 just = User.create!(first_name:"Just", last_name:"Leblanc", email:"just.leblanc@gmail.com", password:"123456")
 file = URI.open(https://image.over-blog.com/3kvxExbDMdbVgTl-ZmbPJuWvIho=/filters:no_upscale()/image%2F0995038%2F20210423%2Fob_bd5b6a_capture-15.png)
-just.photo.attach(io: file filename:"just.png" content_type:"image/png")
+just.photo.attach(io: file, filename:"just.png", content_type:"image/png")
 
 lisa = User.create!(first_name:"Lisa", last_name:"Plenske", email:"lisa.plenske@gmail.com", password:"")
 file = URI.open(https://static1.purebreak.com/articles/8/15/71/98/@/625978-le-destin-de-lisa-que-devient-alexandr-diapo-2.jpg)
-lisa.photo.attach(io: file filename:"lisa.png" content_type:"image/png")
+lisa.photo.attach(io: file, filename:"lisa.png", content_type:"image/png")
 
 laouni = User.create!(first_name:"Laouni", last_name:"Mouhid", email:"laouni.mouhid@gmail.com", password:"123456")
 file = URI.open(https://www.gala.fr/imgre/fit/http.3A.2F.2Fprd2-bone-image.2Es3-website-eu-west-1.2Eamazonaws.2Ecom.2Fprismamedia_people.2F2017.2F06.2F30.2Fc699862d-dd6c-4c2d-936e-0d5acc660d4e.2Ejpeg/100x100/quality/80/la-fouine.jpg)
-laouni.photo.attach(io: file filename:"laouni.png" content_type:"image/png")
+laouni.photo.attach(io: file, filename:"laouni.png", content_type:"image/png")
 
 chen = User.create!(first_name:"Chen", last_name:"Li", email:"chun.li@wanadoo.fr", password:"123456")
 file = URI.open(https://static.wikia.nocookie.net/streetfighter/images/1/15/Chun-Li_animated_movie.png/revision/latest/scale-to-width-down/1086?cb=20180126014842)
-chen.photo.attach(io: file filename:"chen.png" content_type:"image/png")
+chen.photo.attach(io: file, filename:"chen.png", content_type:"image/png")
 
 #TEAMS
 
@@ -76,3 +76,23 @@ event_team5 = EventTeam.create!(event: match5, team: team10)
 puts 'Creating Places...'
 # PLACES
 
+bradys = Place.create!(caracteristic: , adress:"22 Allée Commandant Charcot, 44000 Nantes", name:"Brady's", status:"places disponibles")
+file = URI.open(https://cdn.schlouk-map.com/cache/gallery/uploads/images/places/bradys-62582df31d8dd1.32689537.png)
+brady.photo.attach(io: file, filename:"brady.png", content_type:"image/png")
+
+macbyrne = Place.create!(caracteristic: , adress:"21 Rue des Petites Écuries, 44000 Nantes", name:"John McByrne", status:"places disponibles")
+file = URI.open(https://fastly.4sqi.net/img/general/200x200/11116385_IN7iNR-dN1fQVnDLeOr-HautP4K0lpUP0E5rxduaGGY.jpg)
+macbyrne.photo.attach(io: file, filename:"macbyrne.png", content_type:"image/png")
+
+berlin = Place.create!(caracteristic: , adress:"95 Boulevard Gabriel Lauriol, 44300 Nantes", name:"Berlin 1989", status:"places disponibles")
+file = URI.open(https://mvistatic.com/etablissements/2018-05/16163_crop_640-270_.jpg)
+berlin.photo.attach(io: file, filename:"berlin.png", content_type:"image/png")
+
+lab = Place.create!(caracteristic: , adress:"23 Bd de Chantenay, 44100 Nantes", name:"Little Atlantique Brewery", status:"places disponibles")
+file = URI.open(https://uploads.lebonbon.fr/source/2020/june/2008827/little-atlantique-brewery-nantes_2_400.jpg 400w,https://uploads.lebonbon.fr/source/2020/june/2008827/little-atlantique-brewery-nantes_2_800.jpg 800w,https://uploads.lebonbon.fr/source/2020/june/2008827/little-atlantique-brewery-nantes_2_1200.jpg 1200w,https://uploads.lebonbon.fr/source/2020/june/2008827/little-atlantique-brewery-nantes_2_1600.jpg 1600w)
+lab.photo.attach(io: file, filename:"lab.png", content_type:"image/png")
+
+bureau = Place.create!(caracteristic: , adress:"10 Quai François Mitterrand, 44000 Nantes", name:"Au Bureau
+", status:"places disponibles")
+file = URI.open(/upload/www/images/entites/clients/2472---bar-au-bureau-nantes.jpg)
+bureau.photo.attach(io: file, filename:"bureau.png", content_type:"image/png")
