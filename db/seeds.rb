819f58c7720a2cb818d8799bb1891e69f817e2ff
Ruben HERNANDEZ DOMINGUEZ <113209489+rlucky87@users.noreply.github.com>
2022-11-30 17:34:09 +0100

    showeventend

--- db/seeds.rb
+++ db/seeds.rb
@@ -120,43 +120,43 @@ event_team10 = EventTeam.create!(event: match10, team: team10)
 puts 'Creating Places...'
 # PLACES
 
-LaScierie = Place.create!(characteristic: "🚬🍺🖥️", address: "3 allée de l'Ile Gloriette 44000 Nantes", name:"La Scierie", status:"Places disponibles")
+LaScierie = Place.create!(characteristic: "Venez coupez votre quotidien en rejoignant un lieu de partage", address: "3 allée de l'Ile Gloriette 44000 Nantes", name:"La Scierie", status:"Places disponibles")
 file = URI.open("http://www.lesbarres.com/media/image/slideshow/a1f8f0d08556369fa82d5ce29e775387b7a713e1.jpg")
 LaScierie.photo.attach(io: file, filename: "LaScierie.png", content_type: "image/png")
 
-Brasseurs = Place.create!(characteristic: "🚬🍺🖥️Une cuisine authentique dans l'esprit des anciennes brasseries. De moments généreux et conviviaux à déguster", address: "Centre Commercial Carré 19 Parvis Neptune 44000 Nantes", name:"3 Brasseurs", status:"Places disponibles")
+Brasseurs = Place.create!(characteristic: "Une cuisine authentique dans l'esprit des anciennes brasseries. De moments généreux et conviviaux à déguster", address: "Centre Commercial Carré 19 Parvis Neptune 44000 Nantes", name:"3 Brasseurs", status:"Places disponibles")
 file = URI.open("https://www.radiofrance.fr/s3/cruiser-production/2017/08/ac545240-6ac1-41d6-accd-6193e6b50caa/870x489_dsc_7037.jpg")
 Brasseurs.photo.attach(io: file, filename: "Brasseurs.png", content_type: "image/png")
 
-LeCascabel = Place.create!(characteristic: "🚬🍺🖥️Une belle cascade de joie vous y attend", address: "1 Rue Kervégan 44000 Nantes", name:"Le Cascabel", status:"Places disponibles")
+LeCascabel = Place.create!(characteristic: "Une belle cascade de joie vous y attend", address: "1 Rue Kervégan 44000 Nantes", name:"Le Cascabel", status:"Places disponibles")
 file = URI.open("https://www.google.com/maps/uv?pb=!1s0x4805eea588d9a5c3%3A0x714dea4f8151dd19!3m1!7e115!4shttps%3A%2F%2Flh5.googleusercontent.com%2Fp%2FAF1QipNS5uSIB_Vg6LAdM6BH96qTVFbS6QJeGaYGXI7Q%3Dw260-h175-n-k-no!5sle%20cascabel%20nantes%20-%20Buscar%20con%20Google!15sCgIgAQ&imagekey=!1e10!2sAF1QipPCmEIhZC0IewhtIpnrwRlV5ITmv1IwuErfrwwJ&hl=es&sa=X&ved=2ahUKEwi57vXewNP7AhVsTKQEHT9fCdAQ7ZgBKAB6BAgWEAI#.jpg")
 LeCascabel.photo.attach(io: file, filename: "LeCascabel.png", content_type: "image/png")
 
-LaBelleEquipe = Place.create!(characteristic: "🚬🍺🖥️L'esprit d'équipe que l'on perde ou que l'on gagne",  address: "10 Quai de la Jonelière 44000 Nantes", name:"La belle equipe", status:"Places disponibles")
+LaBelleEquipe = Place.create!(characteristic: "L'esprit d'équipe que l'on perde ou que l'on gagne",  address: "10 Quai de la Jonelière 44000 Nantes", name:"La belle equipe", status:"Places disponibles")
 file = URI.open("https://www.lbe-nantes.fr/i/la-belle-equipe-1877-bar-restaurant-salle-eiffel-rez-de-chausse/3/4/8/1/9/0/1/5/0/2/2/8/8/1548158245_437/5880eee8b302623787de9e0207b33df8.jpg")
 LaBelleEquipe.photo.attach(io: file, filename: "LaBelleEquipe.png", content_type: "image/png")
 
-Giggs = Place.create!(characteristic: "🚬🍺🖥️Un match ne se joue pas que sur la pelouse", address: "4 Pl. Saint-Pierre 44000 Nantes", name:"Gigg's", status:"Places disponibles")
+Giggs = Place.create!(characteristic: "Un match ne se joue pas que sur la pelouse", address: "4 Pl. Saint-Pierre 44000 Nantes", name:"Gigg's", status:"Places disponibles")
 file = URI.open("https://cdn.schlouk-map.com/cache/gallery/uploads/images/places/giggs-irish-pub-625824ac437ab0.58323803.png")
 Giggs.photo.attach(io: file, filename: "LaBelleEquipe.png", content_type: "image/png")
 
-brady = Place.create!(characteristic: "🚬🍺🖥️Près du lieu unique, l'ambiance irlandaise à porter de chope", address: "22 Allée Commandant Charcot, 44000 Nantes", name:"Brady's", status:"places disponibles")
+brady = Place.create!(characteristic: "Près du lieu unique, l'ambiance irlandaise à porter de chope", address: "22 Allée Commandant Charcot, 44000 Nantes", name:"Brady's", status:"places disponibles")
 file = URI.open("https://cdn.schlouk-map.com/cache/gallery/uploads/images/places/bradys-62582df31d8dd1.32689537.png")
 brady.photo.attach(io: file, filename: "brady.png", content_type: "image/png")
 
-macbyrne = Place.create!(characteristic: "🚬🍺🖥️Il n'y a pas qu'en irlande qu'on sait savourer un moment de partage autour d'une pinte", address: "21 Rue des Petites Écuries, 44000 Nantes", name:"John McByrne", status:"places disponibles")
+macbyrne = Place.create!(characteristic: "Il n'y a pas qu'en irlande qu'on sait savourer un moment de partage autour d'une pinte", address: "21 Rue des Petites Écuries, 44000 Nantes", name:"John McByrne", status:"places disponibles")
 file = URI.open("https://fastly.4sqi.net/img/general/200x200/11116385_IN7iNR-dN1fQVnDLeOr-HautP4K0lpUP0E5rxduaGGY.jpg")
 macbyrne.photo.attach(io: file, filename: "macbyrne.png", content_type: "image/png")
 
-berlin = Place.create!(characteristic: "🚬🍺🖥️Lieu de rencontre et de partage reconnu Quoi vous ne connaissez pas ? On vous attend pour vous montrer",  address: "95 Boulevard Gabriel Lauriol, 44300 Nantes", name:"Berlin 1989", status:"places disponibles")
+berlin = Place.create!(characteristic: "Lieu de rencontre et de partage reconnu Quoi vous ne connaissez pas ? On vous attend pour vous montrer",  address: "95 Boulevard Gabriel Lauriol, 44300 Nantes", name:"Berlin 1989", status:"places disponibles")
 file = URI.open("https://mvistatic.com/etablissements/2018-05/16163_crop_640-270_.jpg")
 berlin.photo.attach(io: file, filename: "berlin.png", content_type: "image/png")
 
-lab = Place.create!(characteristic: "🚬🍺🖥️En navibus, voiture, vélo, à pieds ou à cheval, tous les chemin mène au LAB", address: "23 Bd de Chantenay, 44100 Nantes", name:"Little Atlantique Brewery", status:"places disponibles")
+lab = Place.create!(characteristic: "En navibus, voiture, vélo, à pieds ou à cheval, tous les chemin mène au LAB", address: "23 Bd de Chantenay, 44100 Nantes", name:"Little Atlantique Brewery", status:"places disponibles")
 file = URI.open("https://uploads.lebonbon.fr/source/2020/june/2008827/little-atlantique-brewery-nantes_2_400.jpg")
 lab.photo.attach(io: file, filename: "lab.png", content_type: "image/png")
 
-bureau = Place.create!(characteristic: "🚬🍺🖥️Quoi de mieux que de se retrouver au bureau après une journée de travail ?", address: "10 Quai François Mitterrand, 44000 Nantes", name:"Au Bureau", status:"places disponibles")
+bureau = Place.create!(characteristic: "Quoi de mieux que de se retrouver au bureau après une journée de travail ?", address: "10 Quai François Mitterrand, 44000 Nantes", name:"Au Bureau", status:"places disponibles")
 file = URI.open("https://static.actu.fr/uploads/2019/05/55557220_1846310605475174_2848458684785229824_o.jpg")
 bureau.photo.attach(io: file, filename: "bureau.png", content_type: "image/png")
 
