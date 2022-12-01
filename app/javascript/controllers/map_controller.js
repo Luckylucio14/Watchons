c696453271ca602c3ce34862aa718d66c6f3265e
Luc Randriamanantseheno <luckylucio14@gmail.com>
2022-12-01 17:12:37 +0100

    fixfix

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -27,7 +27,7 @@ export default class extends Controller {
       new mapboxgl.Marker()
       .setLngLat([ marker.lng, marker.lat ])
       .addTo(this.map)
-      })
+      
 
       // Create a HTML element for your custom marker
       const customMarker = document.createElement("div")
@@ -42,11 +42,12 @@ export default class extends Controller {
         .setLngLat([marker.lng, marker.lat])
         .setPopup(popup)
         .addTo(this.map)
+      })
     }
-  }
-  #fitMapToMarkers() {
-    const bounds = new mapboxgl.LngLatBounds()
-    this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
-    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
+  
+    #fitMapToMarkers() {
+      const bounds = new mapboxgl.LngLatBounds()
+      this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
+      this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
   }
 }
