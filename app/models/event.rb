bd4973c761f246684244ae82380f11e1c8555c40
Ruben HERNANDEZ DOMINGUEZ <113209489+rlucky87@users.noreply.github.com>
2022-12-01 14:52:50 +0100

    helperdate

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,12 @@ class Event < ApplicationRecord
   has_many :places, through: :event_places
   has_many :event_teams
   has_many :teams, through: :event_teams
+
+  def hour
+    date.strftime("H%:%M")
+  end
+
+  def date
+    date.strftime("d%/%m/%Y")
+  end
 end
