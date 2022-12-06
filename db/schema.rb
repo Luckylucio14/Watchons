e05fcc097284d15e78c2a6e7702442f382246287
Ruben HERNANDEZ DOMINGUEZ <113209489+rlucky87@users.noreply.github.com>
2022-12-06 13:10:27 +0100

    fix seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_01_165553) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_06_111342) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -80,6 +80,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_01_165553) do
     t.float "latitude"
     t.float "longitude"
     t.jsonb "icons", default: [], array: true
+    t.string "city"
   end
 
   create_table "teams", force: :cascade do |t|
