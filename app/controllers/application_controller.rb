ef1b9ef7931dad8a49d6ac1f2fb5c02f9c8c3aed
Luc Randriamanantseheno <luckylucio14@gmail.com>
2022-12-06 17:51:44 +0100

    metatag

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  def default_url_options
+    { host: ENV["watchons.mobi"] || "localhost:3000" }
+  end
 end
 
